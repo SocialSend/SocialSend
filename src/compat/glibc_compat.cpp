21a603e8a13cbe8056f1df07c6ded5eded211765
Remapper <33461506+Remapper@users.noreply.github.com>
2017-11-24 21:53:55 +0000

    Add files via upload

--- src/compat/glibc_compat.cpp
+++ src/compat/glibc_compat.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "config/piex-config.h"
+#include "config/send-config.h"
 #endif
 
 #include <cstddef>

52f74b84ffca2d339be8fab5129b1fd191b4b434
Remapper <33461506+Remapper@users.noreply.github.com>
2017-11-24 21:53:23 +0000

    Add files via upload

--- src/clientversion.h
+++ src/clientversion.h
@@ -41,7 +41,7 @@
 #define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Dash Core Developers, 2015-" STRINGIZE(COPYRIGHT_YEAR) " The SEND Core Developers"
 
 /**
- * piexd-res.rc includes this file, but it cannot cope with real c++ code.
+ * sendd-res.rc includes this file, but it cannot cope with real c++ code.
  * WINDRES_PREPROC is defined to indicate that its pre-processor is running.
  * Anything other than a define should be guarded below.
  */

8017969acb8b00716f90c6062d347e656b350ea8
hernss <hernantravado@yahoo.com.ar>
2018-07-22 22:08:56 -0300

    Add few rpc value
    Add IP:Port report to masternode list
    Add flags report to blockToJSON

--- src/rpcmasternode.cpp
+++ src/rpcmasternode.cpp
@@ -295,6 +295,7 @@ Value listmasternodes(const Array& params, bool fHelp)
             obj.push_back(Pair("outidx", (uint64_t)oIdx));
             obj.push_back(Pair("status", strStatus));
             obj.push_back(Pair("addr", CBitcoinAddress(mn->pubKeyCollateralAddress.GetID()).ToString()));
+            obj.push_back(Pair("ip:port", mn->addr.ToString()));
             obj.push_back(Pair("version", mn->protocolVersion));
             obj.push_back(Pair("lastseen", (int64_t)mn->lastPing.sigTime));
             obj.push_back(Pair("activetime", (int64_t)(mn->lastPing.sigTime - mn->sigTime)));

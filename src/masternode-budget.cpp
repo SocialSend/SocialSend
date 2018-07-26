4b947f570afd1c7627877f8b57bb7d1025bcc805
Hernan T <hernantravado@yahoo.com.ar>
2018-07-26 19:53:17 -0300

    Change Budget rewards

--- src/masternode-budget.cpp
+++ src/masternode-budget.cpp
@@ -834,44 +834,14 @@ CAmount CBudgetManager::GetTotalBudget(int nHeight)
     if (chainActive.Tip() == NULL) return 0;
 
     if (Params().NetworkID() == CBaseChainParams::TESTNET) {
-        CAmount nSubsidy = 500 * COIN;
-        return ((nSubsidy / 100) * 10) * 146;
+        CAmount nSubsidy = 5000 * COIN;
+        return ((nSubsidy / 100) * 10) * 146; //max budget for testnet 73000
     }
-
-  //get block value and calculate from that
-    CAmount nSubsidy = 0;
-    if (nHeight <= Params().LAST_POW_BLOCK() && nHeight >= 2000) {
-        nSubsidy = 50 * COIN;
-    } else if (nHeight <= 172000 && nHeight > Params().LAST_POW_BLOCK()) {
-        nSubsidy = 45 * COIN;
-    } else if (nHeight <= 345599 && nHeight >= 302400) {
-        nSubsidy = 42 * COIN;
-    } else if (nHeight <= 388799 && nHeight >= 345600) {
-        nSubsidy = 40 * COIN;
-    } else if (nHeight <= 431999 && nHeight >= 388800) {
-        nSubsidy = 39 * COIN;
-    } else if (nHeight <= 475199 && nHeight >= 432000) {
-        nSubsidy = 38 * COIN;
-    } else if (nHeight <= 518399 && nHeight >= 475200) {
-        nSubsidy = 38 * COIN;
-    } else if (nHeight <= 561599 && nHeight >= 518400) {
-        nSubsidy = 37 * COIN;
-    } else if (nHeight <= 604799 && nHeight >= 561600) {
-        nSubsidy = 36 * COIN;
-    } else if (nHeight <= 647999 && nHeight >= 604800) {
-        nSubsidy = 35 * COIN;
-    } else if (nHeight >= 648000) {
-        nSubsidy = 35 * COIN;
-    } else {
-        nSubsidy = 0 * COIN;
-    }
-
-    // Amount of blocks in a months period of time (using 1 minutes per) = (60*24*30)
-    if (nHeight <= 172800) {
-return 648000 * COIN;
-    } else {
-        return ((nSubsidy / 100) * 10) * 1440 * 30;
-    }
+    
+	//get block value and calculate from that
+    CAmount nSubsidy = GetBlockValue(chainActive.Tip()->nHeight);
+	return ((nSubsidy / 100) * 10) * 1440 * 30;   //10% of one month reward
+    
 }
 
 void CBudgetManager::NewBlock()

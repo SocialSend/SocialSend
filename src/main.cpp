7f58b98b2fe4ee57339e9e9626d7df6aa48a1c63
Jon <squid@sqdmc.net>
2020-06-19 15:19:36 -0400

    Spork Changes for ActiveProtocol Prepare version

--- src/main.cpp
+++ src/main.cpp
@@ -5767,25 +5767,20 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 //       it was the one which was commented out
 int ActiveProtocol()
 {
-    // SPORK_14 was used for 70710. Leave it 'ON' so they don't see < 70710 nodes. They won't react to SPORK_15
-    // messages because it's not in their code
-    /*
+    // SPORK_14 is used for 70820. Nodes < 70820 don't see it and still get their protocol version via SPORK_15
     if (IsSporkActive(SPORK_14_NEW_PROTOCOL_ENFORCEMENT)) {
         if (chainActive.Tip()->nHeight >= Params().ModifierUpgradeBlock())
             return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
     }
 
     return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;
-*/
 
-
-    // SPORK_15 is used for 70910. Nodes < 70910 don't see it and still get their protocol version via SPORK_14 and their
-    // own ModifierUpgradeBlock()
-
-    if (IsSporkActive(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2))
+    // SPORK_15 was used for 70810. Leave it 'OFF' so they see >= 70810 nodes.
+    //  Nodes > 70810 won't react to SPORK_14 messages beacuse it is not in their code.
+    /*if (IsSporkActive(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2))
         return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
 
-    return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;
+    return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;*/
 }
 
 // requires LOCK(cs_vRecvMsg)
